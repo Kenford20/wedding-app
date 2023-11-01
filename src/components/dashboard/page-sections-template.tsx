aa405be9084c2724018c0df72b21c411dc2e2dcc
Kenny Zhou <kzhou0066@gmail.com>
2023-11-01 01:13:22 -0500

    modularize and style dashboard landing page with page section templates

---
+++ src/components/dashboard/page-sections-template.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import { AiOutlineDown, AiOutlinePlusCircle } from 'react-icons/ai';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { sharedStyles } from '../shared-styles';
+
+type PageSectionsTemplateProps = {
+  title: string;
+  children?: React.ReactNode;
+};
+
+export default function PageSectionsTemplate({
+  title,
+  children,
+}: PageSectionsTemplateProps) {
+  return (
+    <div className='w-full border'>
+      <div className='flex justify-between px-5 py-7'>
+        <div className='flex'>
+          <button>
+            <AiOutlineDown />
+          </button>
+          <h2 className='ml-3 text-xl font-semibold'>{title}</h2>
+        </div>
+        <div className='flex'>
+          <Link href='/dashboard/preview'>
+            <button className='text-pink-400'>Preview</button>
+          </Link>
+          <span className={sharedStyles.verticalDivider}>|</span>
+          <BsThreeDotsVertical size={23} />
+        </div>
+      </div>
+      {children ?? (
+        <>
+          <div className='border-t'></div>
+          <div className='p-5'>
+            <div className='flex'>
+              <AiOutlinePlusCircle
+                size={25}
+                color={sharedStyles.primaryColorHex}
+              />
+              <p className={`pl-3 text-${sharedStyles.primaryColor}`}>
+                Add More to {title}
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,14 @@
+import { useEffect, useState } from 'react';
+import { api } from '~/utils/api';
+import { sharedStyles } from '~/components/shared-styles';
+import { LoadingPage } from '~/components/loader';
+import { BiCollapseVertical } from 'react-icons/bi';
+import { HiOutlineArrowsUpDown } from 'react-icons/hi2';
 import Link from 'next/link';
 import Layout from '../layout';
-import { api } from '~/utils/api';
-import { LoadingPage } from '~/components/loader';
 import DashboardHeader from '~/components/dashboard/website-header';
 import RegistrySetup from '~/components/dashboard/registry-setup';
-import { useEffect, useState } from 'react';
-import { sharedStyles } from '~/components/shared-styles';
+import PageSectionsTemplate from '~/components/dashboard/page-sections-template';
 
 export default function Dashboard() {
   const [showRegistrySetup, setShowRegistrySetup] = useState(true);
@@ -38,68 +41,128 @@ export default function Dashboard() {
           </section>
         )}
         <br />
-        <h1>Pages</h1>
-        <section>
-          <h1>Home</h1>
-          <h2>Name1 & Name2</h2>
-          <i>Icon</i>
-          <span>WeddingDate</span>
-          <span>|</span>
-          <span>daysRemaining!</span>
-          <i>Icon</i>
-          <span>
-            <button>Add your wedding location</button>
-          </span>
-          <h3>Events (map through them here)</h3>
-          <div>
-            <button>Edit Icon Button</button>
-            <h2>Rehearsal Dinner</h2>
-            <i>Icon</i>
-            <span>eventDate</span>
-            <i>Icon</i>
-            <span>eventTime</span>
-            <i>Icon</i>
-            <span>eventVenue</span>
-          </div>
-        </section>
-        <section>
-          <div>
-            <h1>RSVP</h1>
-            <div>Rehearsal Dinner</div>
-          </div>
-          <div>
-            <div>
-              <h2>Wedding Day</h2>
-              <i>Icon</i>
-              <span>numGuestsInvited Guests Invited</span>
-            </div>
-            <div>
-              <h3>Will you be attending?</h3>
-              <div>
-                some chart
-                <p>numGuestsResponded of numGuestsInvited responded</p>
-              </div>
-              <div>
-                <div>
-                  <span>Accepted</span>
-                  <span>numGuestsAccepted</span>
+        <div className='grid grid-cols-[4fr_1fr] gap-5 px-16'>
+          <section className=''>
+            <div className='mb-5 flex justify-between'>
+              <h2 className='text-xl font-semibold'>Pages</h2>
+              <div className='flex items-center'>
+                <div className='flex cursor-pointer'>
+                  <HiOutlineArrowsUpDown
+                    size={21}
+                    color={sharedStyles.primaryColorHex}
+                  />
+                  <button className={`text-${sharedStyles.primaryColor} mx-2`}>
+                    Reorder
+                  </button>
                 </div>
-                <div>
-                  <span>Declined</span>
-                  <span>numGuestsDeclined</span>
-                </div>
-                <div>
-                  <span>No Response</span>
-                  <span>numGuestsNoResponse</span>
+                <div className='flex cursor-pointer'>
+                  <BiCollapseVertical
+                    size={21}
+                    color={sharedStyles.primaryColorHex}
+                  />
+                  <button className={`text-${sharedStyles.primaryColor}`}>
+                    Collapse All
+                  </button>
                 </div>
               </div>
             </div>
-            <Link href='/guest-list'>Manage Guest List</Link>
-          </div>
-          <div>
-            <h3>General Questions</h3>
-          </div>
-        </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'Home'}>
+                <>
+                  <h2>Home</h2>
+                  <h2>Name1 & Name2</h2>
+                  <i>Icon</i>
+                  <span>WeddingDate</span>
+                  <span>|</span>
+                  <span>daysRemaining!</span>
+                  <i>Icon</i>
+                  <span>
+                    <button>Add your wedding location</button>
+                  </span>
+                  <h3>Events (map through them here)</h3>
+                  <div>
+                    <button>Edit Icon Button</button>
+                    <h2>Rehearsal Dinner</h2>
+                    <i>Icon</i>
+                    <span>eventDate</span>
+                    <i>Icon</i>
+                    <span>eventTime</span>
+                    <i>Icon</i>
+                    <span>eventVenue</span>
+                  </div>
+                </>
+              </PageSectionsTemplate>
+            </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'Our Story'} />
+            </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'Wedding Party'} />
+            </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'Photos'} />
+            </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'Q + A'} />
+            </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'Travel'} />
+            </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'Things to Do'} />
+            </section>
+            <section className='mb-10'>
+              <PageSectionsTemplate title={'RSVP'}>
+                <>
+                  <div>
+                    <h2>RSVP</h2>
+                    <div>Rehearsal Dinner</div>
+                  </div>
+                  <div>
+                    <div>
+                      <h2>Wedding Day</h2>
+                      <i>Icon</i>
+                      <span>numGuestsInvited Guests Invited</span>
+                    </div>
+                    <div>
+                      <h3>Will you be attending?</h3>
+                      <div>
+                        some chart
+                        <p>numGuestsResponded of numGuestsInvited responded</p>
+                      </div>
+                      <div>
+                        <div>
+                          <span>Accepted</span>
+                          <span>numGuestsAccepted</span>
+                        </div>
+                        <div>
+                          <span>Declined</span>
+                          <span>numGuestsDeclined</span>
+                        </div>
+                        <div>
+                          <span>No Response</span>
+                          <span>numGuestsNoResponse</span>
+                        </div>
+                      </div>
+                    </div>
+                    <Link href='/guest-list'>Manage Guest List</Link>
+                  </div>
+                  <div>
+                    <h3>General Questions</h3>
+                  </div>
+                </>
+              </PageSectionsTemplate>
+            </section>
+          </section>
+
+          <section className='h-72 w-72 bg-red-200'>
+            <div className='flex justify-between'>
+              <h2 className='text-xl font-semibold'>Your Theme</h2>
+              <button>Browse Themes</button>
+            </div>
+            <div>theme</div>
+          </section>
+        </div>
       </main>
     </Layout>
   );
