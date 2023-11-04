2413692afe0c1fa8915b125450bfd7a2f685dbb7
Kenny Zhou <kzhou0066@gmail.com>
2023-11-04 01:31:12 -0500

    add api route for fetching the wedding data to populate user website UI

--- src/pages/websites/[subroute].tsx
+++ src/pages/websites/[subroute].tsx
@@ -9,50 +9,58 @@ export default function WebsiteTemplate() {
   const router = useRouter();
   const websiteUrl = router.query.subroute;
 
-  const { data: doesWebsiteExist, isLoading } = api.website.find.useQuery({
-    websiteUrl: String(router.query.subroute!),
-  });
+  const { data: weddingData, isLoading } =
+    api.website.fetchWeddingData.useQuery({
+      subUrl: String(router.query.subroute!),
+    });
 
   if (isLoading) return <LoadingSpinner />;
+  if (!weddingData) return <div>404</div>;
 
-  return doesWebsiteExist ? (
-    <main className='flex h-screen flex-col items-center justify-center gap-20'>
-      <p>{websiteUrl}</p>
-      <div className='text-center'>
-        <h1 className='text-3xl'>*firstName & partnerFirstName</h1>
-        <p className='text-md'>*weddingDate: October 15, 2024</p>
-        <p className='text-lg'>*remaininigDays: 413 Days To Go!</p>
-      </div>
+  console.log('wedding, ', weddingData);
 
-      <div className='text-center'>
-        image placeholder will need to look into uploading on dashboard and
-        storing in some storage bucket to be fetched and rendered here s3
-        bucket?
-      </div>
+  return (
+    !!weddingData && (
+      <main className='flex h-screen flex-col items-center justify-center gap-20'>
+        <p>{websiteUrl}</p>
+        <div className='text-center'>
+          <h1 className='text-3xl'>
+            {weddingData.groomFirstName} & {weddingData.brideFirstName}
+          </h1>
+          <p className='text-md'>*weddingDate: October 15, 2024</p>
+          <p className='text-lg'>*remaininigDays: 413 Days To Go!</p>
+        </div>
 
-      <div className='text-center'>
-        <button className='rounded-md bg-pink-200 px-10 py-4'>RSVP</button>
-        <h2>Wedding Day</h2>
-        <p className='text-xl'>*weddingDate: October 15, 2024</p>
-      </div>
+        <div className='text-center'>
+          image placeholder will need to look into uploading on dashboard and
+          storing in some storage bucket to be fetched and rendered here s3
+          bucket?
+        </div>
 
-      <div className='text-center'>
-        <h1 className='text-3xl'>
-          *firstNameInitial & partnerFirstNameInitial
-        </h1>
-        <br />
-        <p className='text-lg'>*weddingDate numberFormat: 10.15.2024</p>
-      </div>
+        <div className='text-center'>
+          <button className='rounded-md bg-pink-200 px-10 py-4'>RSVP</button>
+          <h2>Wedding Day</h2>
+          <p className='text-xl'>*weddingDate: October 15, 2024</p>
+        </div>
 
-      <div className='text-center'>
-        <p className='text-sm'>Created by Kenford</p>
-        <p className='text-sm'>
-          Getting married?
-          <Link href={'/'}>Create your wedding website for free</Link>
-        </p>
-      </div>
-    </main>
-  ) : (
-    <div>404</div>
+        <div className='text-center'>
+          <h2 className='text-3xl'>
+            {weddingData.groomFirstName[0]} & {weddingData.brideFirstName[0]}
+          </h2>
+          <br />
+          <p className='text-lg'>*weddingDate numberFormat: 10.15.2024</p>
+        </div>
+
+        <div className='text-center'>
+          <p className='text-sm'>Created by Kenford</p>
+          <p className='text-sm'>
+            Getting married?{' '}
+            <span className='underline'>
+              <Link href={'/'}>Create your wedding website for free</Link>
+            </span>
+          </p>
+        </div>
+      </main>
+    )
   );
 }
--- src/server/api/routers/websites.ts
+++ src/server/api/routers/websites.ts
@@ -95,4 +95,59 @@ export const websitesRouter = createTRPCRouter({
       });
       return !!website;
     }),
+
+  fetchWeddingData: publicProcedure
+    .input(z.object({ subUrl: z.string() }))
+    .query(async ({ ctx, input }) => {
+      if (!ctx.userId) return;
+
+      const currentWebsite = await ctx.prisma.website.findFirst({
+        where: {
+          subUrl: input.subUrl,
+        },
+      });
+
+      if (!currentWebsite) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'This website does not exist.',
+        });
+      }
+
+      type TweddingUser = {
+        groomFirstName: string;
+        groomLastName: string;
+        brideFirstName: string;
+        brideLastName: string;
+      };
+
+      const weddingUser: TweddingUser | null = await ctx.prisma.user.findFirst({
+        where: {
+          id: currentWebsite.userId,
+        },
+      });
+
+      if (!weddingUser) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Failed to fetch wedding website data.',
+        });
+      }
+
+      const events = await ctx.prisma.event.findMany({
+        where: {
+          userId: ctx.userId,
+        },
+      });
+
+      const weddingData = {
+        groomFirstName: weddingUser.groomFirstName,
+        groomLastName: weddingUser.groomLastName,
+        brideFirstName: weddingUser.brideFirstName,
+        brideLastName: weddingUser.brideLastName,
+        events,
+      };
+
+      return weddingData;
+    }),
 });
