b2fd49610c7af7fc587be52e5d7fb53834cc4101
Kenny Zhou <kzhou0066@gmail.com>
2023-11-08 01:35:00 -0600

    style the add guest form

--- src/components/guest-list/add-guest-form.tsx
+++ src/components/guest-list/add-guest-form.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { api } from '~/utils/api';
 import { LoadingSpinner } from '../loader';
+import { sharedStyles } from '../shared-styles';
 
 type Event = {
   id: string;
@@ -76,16 +77,18 @@ export default function AddGuestForm({
   };
 
   return (
-    <div className='absolute top-0 flex h-screen w-screen justify-end bg-transparent/[0.5]'>
+    <div
+      id='foobar'
+      className='absolute top-0 flex h-screen w-screen justify-end bg-transparent/[0.5]'
+    >
       {isCreatingGuest && (
         <div className='flex items-center justify-center'>
           <LoadingSpinner />
         </div>
       )}
-      <div></div>
-      <div className='h-screen w-1/3 bg-pink-200'>
-        <div className='bottom-2 border'>
-          <h1>Add Party</h1>
+      <div className='h-fit w-[500px] bg-white'>
+        <div className='flex justify-between border-b p-5'>
+          <h1 className='text-xl font-semibold'>Add Party</h1>
           <span
             className='cursor-pointer'
             onClick={() => setShowGuestForm(false)}
@@ -93,95 +96,138 @@ export default function AddGuestForm({
             X
           </span>
         </div>
-        <div>
-          <h2>Guest Name</h2>
-          <input
-            placeholder='First Name'
-            value={guestFormData.firstName}
-            onChange={(e) => handleOnChange('firstName', e.target.value)}
-          />
-          <input
-            placeholder='Last Name'
-            value={guestFormData.lastName}
-            onChange={(e) => handleOnChange('lastName', e.target.value)}
-          />
+        <div className='p-5'>
+          <h2 className='mb-3 text-xl font-semibold'>Guest Name</h2>
+          <div className='flex justify-between gap-3'>
+            <input
+              className='w-1/2 border p-3'
+              placeholder='First Name'
+              value={guestFormData.firstName}
+              onChange={(e) => handleOnChange('firstName', e.target.value)}
+            />
+            <input
+              className='w-1/2 border p-3'
+              placeholder='Last Name'
+              value={guestFormData.lastName}
+              onChange={(e) => handleOnChange('lastName', e.target.value)}
+            />
+          </div>
         </div>
-        <div>
-          <h3>Invite to the following events:</h3>
-          {events?.map((event) => {
-            return (
-              <div key={event.id} className='inline-block'>
-                <input
-                  type='checkbox'
-                  id={event.id}
-                  onChange={(e) => handleSelectEvent(e, event)}
-                />
-                <label htmlFor={event.id}>{event.name}</label>
-              </div>
-            );
-          })}
+        <div className='p-5'>
+          <h3 className='mb-3 text-gray-400'>
+            Invite to the following events:
+          </h3>
+          <div className='grid grid-cols-2 gap-3'>
+            {events?.map((event) => {
+              return (
+                <div key={event.id} className=''>
+                  <div className='flex items-center gap-3'>
+                    <input
+                      className={`h-6 w-6 border p-3 accent-${sharedStyles.primaryColor}`}
+                      type='checkbox'
+                      id={event.id}
+                      onChange={(e) => handleSelectEvent(e, event)}
+                    />
+                    <label htmlFor={event.id}>{event.name}</label>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <button>+ Add A Guest To This Party</button>
-        <div>
-          <h2>Contact Information</h2>
-          <input
-            placeholder='Street Address'
-            value={guestFormData.address1}
-            onChange={(e) => handleOnChange('address1', e.target.value)}
-          />
-          <input
-            placeholder='Apt/Suite/Other'
-            value={guestFormData.address2}
-            onChange={(e) => handleOnChange('address2', e.target.value)}
-          />
-          <input
-            placeholder='City'
-            value={guestFormData.city}
-            onChange={(e) => handleOnChange('city', e.target.value)}
-          />
-          <select
-            value={guestFormData.state}
-            onChange={(e) => handleOnChange('state', e.target.value)}
-          >
-            <option defaultValue='State'>State</option>
-            <option>AL</option>
-            <option>AR</option>
-            <option>WY</option>
-          </select>
-          <input
-            placeholder='Zip Code'
-            value={guestFormData.zipCode}
-            onChange={(e) => handleOnChange('zipCode', e.target.value)}
-          />
-          <select
-            value={guestFormData.country}
-            onChange={(e) => handleOnChange('country', e.target.value)}
-          >
-            <option defaultValue='State'>Country</option>
-            <option>Murca</option>
-            <option>Mexico</option>
-            <option>Canada</option>
-          </select>
-          <input
-            placeholder='Phone'
-            value={guestFormData.phoneNumber}
-            onChange={(e) => handleOnChange('phoneNumber', e.target.value)}
-          />
-          <input
-            placeholder='Email'
-            value={guestFormData.email}
-            onChange={(e) => handleOnChange('email', e.target.value)}
-          />
-          <h2>My Notes</h2>
+        <div className='mt-3 text-center'>
+          <button className={`text-${sharedStyles.primaryColor}`}>
+            + Add A Guest To This Party
+          </button>
+        </div>
+        <div className='p-5'>
+          <h2 className='mb-3 text-xl font-semibold'>Contact Information</h2>
+          <div className='grid grid-cols-1 grid-rows-[repeat(5,50px)] gap-3'>
+            <input
+              className='w-100 border p-3'
+              placeholder='Street Address'
+              value={guestFormData.address1}
+              onChange={(e) => handleOnChange('address1', e.target.value)}
+            />
+            <input
+              className='w-100 border p-3'
+              placeholder='Apt/Suite/Other'
+              value={guestFormData.address2}
+              onChange={(e) => handleOnChange('address2', e.target.value)}
+            />
+            <div className='flex gap-3'>
+              <input
+                className='w-1/2 border p-3'
+                placeholder='City'
+                value={guestFormData.city}
+                onChange={(e) => handleOnChange('city', e.target.value)}
+              />
+              <select
+                value={guestFormData.state}
+                onChange={(e) => handleOnChange('state', e.target.value)}
+                className='w-1/4 border p-3'
+              >
+                <option defaultValue='State'>State</option>
+                <option>AL</option>
+                <option>AR</option>
+                <option>WY</option>
+              </select>
+              <input
+                className='w-1/4 border p-3'
+                placeholder='Zip Code'
+                value={guestFormData.zipCode}
+                onChange={(e) => handleOnChange('zipCode', e.target.value)}
+              />
+            </div>
+            <select
+              className='w-100 border p-3'
+              value={guestFormData.country}
+              onChange={(e) => handleOnChange('country', e.target.value)}
+            >
+              <option defaultValue='State'>Country</option>
+              <option>Murca</option>
+              <option>Mexico</option>
+              <option>Canada</option>
+            </select>
+            <div className='flex gap-3'>
+              <input
+                className='w-1/2 border p-3'
+                placeholder='Phone'
+                value={guestFormData.phoneNumber}
+                onChange={(e) => handleOnChange('phoneNumber', e.target.value)}
+              />
+              <input
+                className='w-1/2 border p-3'
+                placeholder='Email'
+                value={guestFormData.email}
+                onChange={(e) => handleOnChange('email', e.target.value)}
+              />
+            </div>
+          </div>
+          <h2 className='my-5 text-xl font-semibold'>My Notes</h2>
           <textarea
             placeholder='Enter notes about your guests, like food allergies'
             value={guestFormData.notes}
             onChange={(e) => handleOnChange('notes', e.target.value)}
+            className='h-32 w-full border p-3'
+            style={{ resize: 'none' }}
           />
         </div>
-        <div>
-          <button onClick={() => setShowGuestForm(false)}>Cancel</button>
+        <div className='flex gap-3 p-5'>
           <button
+            onClick={() => setShowGuestForm(false)}
+            className={`${sharedStyles.secondaryButton({
+              px: 'px-12',
+              py: 'py-2',
+            })} w-1/2`}
+          >
+            Cancel
+          </button>
+          <button
+            className={`${sharedStyles.primaryButton({
+              px: 'px-12',
+              py: 'py-2',
+            })} w-1/2`}
             onClick={() =>
               mutate({ ...guestFormData, eventIds: selectedEvents })
             }
