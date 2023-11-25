54575471321b91dc8a5660244f2f77663cc70933
Kenny Zhou <kzhou0066@gmail.com>
2023-11-25 03:54:07 -0600

    refactor guest table UI to render rows based on households instead of individual guests

--- src/components/guest-list/guest-table.tsx
+++ src/components/guest-list/guest-table.tsx
@@ -2,8 +2,68 @@ import { AiOutlineHome } from 'react-icons/ai';
 import { CiMail } from 'react-icons/ci';
 import { FaSort } from 'react-icons/fa';
 import { HiOutlinePhone } from 'react-icons/hi2';
-import { type Household, type Event } from '~/types/schema';
+import { type Household, type Event, type Guest } from '~/types/schema';
 import { sharedStyles } from '../shared-styles';
+
+type TableRowProps = {
+  guest: Guest;
+  events: Event[];
+  partySize: number;
+  notes: string;
+  foobar: boolean;
+};
+
+const TableRow = ({
+  guest,
+  events,
+  partySize,
+  notes,
+  foobar,
+}: TableRowProps) => {
+  if (!guest) return null;
+  return (
+    <div
+      key={guest.id}
+      className={`guest-table grid min-w-fit items-center gap-12 pl-8`}
+      style={{
+        gridTemplateColumns: `40px 240px 100px 125px repeat(${events.length}, 175px) 175px`,
+      }}
+    >
+      <div>
+        <input
+          className='h-6 w-6 cursor-pointer border-gray-100'
+          style={{ accentColor: sharedStyles.primaryColorHex }}
+          type='checkbox'
+          id={`check-guest-${guest.id}`}
+        />
+      </div>
+      <h3>{`${guest.firstName} ${guest.lastName}`}</h3>
+      {foobar ? <span className='absolue'>{partySize}</span> : <div></div>}
+      {foobar ? (
+        <div className='flex gap-2'>
+          <AiOutlineHome size={22} />
+          <HiOutlinePhone size={22} />
+          <CiMail size={23} />
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {events?.map((event) => {
+        return (
+          <div key={event.id}>
+            <select name='guestRSVP' id={`guest-rsvp-${guest.id}`}>
+              <option value='Not Invited'>Not Invited</option>
+              <option value='Invited'>Invited</option>
+              <option value='Attending'>Attending</option>
+              <option value='Declined'>Declined</option>
+            </select>
+          </div>
+        );
+      })}
+      <div>{foobar && notes}</div>
+    </div>
+  );
+};
 
 type GuestTableProps = {
   events: Event[];
@@ -13,9 +73,9 @@ type GuestTableProps = {
 export default function GuestTable({ events, households }: GuestTableProps) {
   return (
     <div className={sharedStyles.desktopPaddingSidesGuestList}>
-      <div className={`box-border overflow-auto`}>
+      <div className='box-border max-h-[80vh] overflow-auto'>
         <div
-          className={`guest-table grid min-w-fit items-center gap-12 border-b py-6 pl-8 font-light italic`}
+          className='guest-table grid min-w-fit items-center gap-12 border-b py-6 pl-8 font-light italic'
           style={{
             gridTemplateColumns: `40px 240px 100px 125px repeat(${events.length}, 175px) 175px`,
           }}
@@ -25,7 +85,7 @@ export default function GuestTable({ events, households }: GuestTableProps) {
               style={{ accentColor: sharedStyles.primaryColorHex }}
               type='checkbox'
               id='check-all'
-              className='h-6 w-6'
+              className='h-6 w-6 cursor-pointer'
             />
           </div>
           <div className='flex cursor-pointer items-center gap-2'>
@@ -45,46 +105,23 @@ export default function GuestTable({ events, households }: GuestTableProps) {
 
         <div className='text-md min-w-fit border-l border-r'>
           {households?.map((household) => {
-            return household.guests.map((guest) => {
-              return (
-                <div
-                  key={guest.id}
-                  className={`guest-table grid min-w-fit items-center gap-12 border-b py-5 pl-8`}
-                  style={{
-                    gridTemplateColumns: `40px 240px 100px 125px repeat(${events.length}, 175px) 175px`,
-                  }}
-                >
-                  <div>
-                    <input
-                      className='h-6 w-6'
-                      style={{ accentColor: sharedStyles.primaryColorHex }}
-                      type='checkbox'
-                      id={`check-guest-${guest.id}`}
+            return (
+              // TODO: refactor table layout for households with multiple guests
+              <div key={household.id} className='border-b py-5'>
+                {household.guests.map((guest, i) => {
+                  return (
+                    <TableRow
+                      key={guest.id}
+                      guest={guest}
+                      events={events}
+                      partySize={household.guests.length}
+                      notes={household.notes ?? '-'}
+                      foobar={i === 0}
                     />
-                  </div>
-                  <h3>{`${guest.firstName} ${guest.lastName}`}</h3>
-                  <span>1</span>
-                  <div className='flex gap-2'>
-                    <AiOutlineHome size={22} />
-                    <HiOutlinePhone size={22} />
-                    <CiMail size={23} />
-                  </div>
-                  {events?.map((event) => {
-                    return (
-                      <div key={event.id}>
-                        <select name='guestRSVP' id={`guest-rsvp-${guest.id}`}>
-                          <option value='Not Invited'>Not Invited</option>
-                          <option value='Invited'>Invited</option>
-                          <option value='Attending'>Attending</option>
-                          <option value='Declined'>Declined</option>
-                        </select>
-                      </div>
-                    );
-                  })}
-                  <div>{guest.notes ?? '-'}</div>
-                </div>
-              );
-            });
+                  );
+                })}
+              </div>
+            );
           })}
         </div>
       </div>
